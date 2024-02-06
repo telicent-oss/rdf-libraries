9bed1e02fd48b3b354eef7ef5209e025070c69a8
Dave Cockrell <dave@telicent.io>
2024-02-06 13:22:23 +0000

    fix chalk import problem

--- scripts/enforcePnpm.js
+++ scripts/enforcePnpm.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
-const chalk = require('chalk');
-if (!/pnpm/.test(process.env.npm_execpath)) {
-  console.error(chalk.redBright('You must use pnpm to install dependencies:'));
-  console.error('  $ pnpm install');
-  process.exit(1);
-}
+import('chalk').then(chalk => {
+  if (!/pnpm/.test(process.env.npm_execpath)) {
+    console.error(chalk.redBright('You must use pnpm to install dependencies:'));
+    console.error('  $ pnpm install');
+    process.exit(1);
+  }
+}).catch(error => {
+  console.error('Error loading chalk', error)
+})
