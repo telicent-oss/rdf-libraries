9e8adcd24b8fc47bf1aa1d8439bda9ab41ffbdd3
Dave Cockrell <dave@telicent.io>
2024-08-02 14:54:16 +0100

    fix: remove chevron

--- packages/OntologyService/index.ts
+++ packages/OntologyService/index.ts
@@ -1170,6 +1170,7 @@ export class OntologyService extends RdfService {
         if (statement.labels) {
           node.rdfsLabels = statement.labels.value.split("||")
         }
+
         if (statement.styles) {
           const stArray = statement.styles.value.split("||")
           try {
