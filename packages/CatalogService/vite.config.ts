1624a5d30b2f6502c9981f954885d5b4a5273c7f
ACoolmanTelicent <a.coleman@telicent.io>
2024-08-13 17:46:53 +0100

    test

--- packages/CatalogService/vite.config.ts
+++ packages/CatalogService/vite.config.ts
@@ -2,7 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
-module.exports = defineConfig({
+export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'index.ts'),
