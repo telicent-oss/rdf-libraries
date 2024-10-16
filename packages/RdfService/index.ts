4b4b546b136309e208b1cba99633ea0e54e2dd9d
Dave Cockrell <dave@telicent.io>
2024-10-16 17:11:02 +0100

    fix: add post request for sparql queries

--- packages/RdfService/index.ts
+++ packages/RdfService/index.ts
@@ -842,7 +842,15 @@ export class RdfService {
   */
   async runQuery<T>(query: string): Promise<QueryResponse<T>> {
     if (isEmptyString(query)) throw Error("runQuery: A valid query is required");
-    const response = await fetch(this.queryEndpoint + encodeURIComponent(this.sparqlPrefixes + query), { 'headers': { 'Expects': 'application/sparql-results+json' } })
+    const response = await fetch(this.queryEndpoint, {
+      method: 'POST',
+      headers: {
+        'Expects': 'application/sparql-results+json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/sparql-results+json',
+      },
+      body: new URLSearchParams({ query })
+    })
     if (!response.ok) {
       throw response.statusText
     }
