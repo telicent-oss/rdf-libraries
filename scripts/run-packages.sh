548df61137038b2064bb0687cde418b706f5c293
ACoolmanTelicent <a.coleman@telicent.io>
2024-12-05 10:42:27 +0000

    fix(run-packages): --verbose

--- scripts/run-packages.sh
+++ scripts/run-packages.sh
@@ -7,5 +7,5 @@ shift
 # To get consistent package.json scripts, must exclude root package to avoid nx infinite loops
 if [ -f nx.json ]; then
   PACKAGE_NAME=$(node -e "console.log(require('./package.json').name)")
-  nx run-many --target=$TARGET --all --exclude=$PACKAGE_NAME $@;
+  nx run-many --target=$TARGET --all --verbose --exclude=$PACKAGE_NAME $@;
 fi
