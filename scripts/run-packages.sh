85feb15c594e1a6cbeaad1e8c26dd9fa5304be48
ACoolmanTelicent <a.coleman@telicent.io>
2024-12-11 11:44:13 +0000

    ci: stability

--- scripts/run-packages.sh
+++ scripts/run-packages.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
-set -e
+# set -e  # Exit on error
+# set -x  # Print commands and their arguments as they are executed
+
+error_handler() {
+  echo "Error occurred at $(date), while executing command: $BASH_COMMAND"
+}
+
+trap 'error_handler' ERR
 
 TARGET=$1
 shift
 
-# To get consistent package.json scripts, must exclude root package to avoid nx infinite loops
+# Check if inside an Nx workspace
 if [ -f nx.json ]; then
-  PACKAGE_NAME=$(node -e "console.log(require('./package.json').name)")
-  nx run-many --target=$TARGET --all --exclude=$PACKAGE_NAME $@;
+  # Extract the root package name to exclude it from nx run-many to prevent infinite loops
+  PACKAGE_NAME=$(node -e "try { console.log(require('./package.json').name); } catch(e) { console.error('Failed to extract package name:', e); process.exit(1); }")
+  
+  # Run Nx command for all packages except the root, passing additional arguments if provided
+  # TODO verify --parallel=1 --skipNxCache=true
+  # WHY added to try get CI to pass
+  nx run-many --target="$TARGET" --all --verbose --parallel=1 --skipNxCache=true --exclude="$PACKAGE_NAME" "$@";
+else
+  echo "Not an Nx workspace (nx.json not found), skipping script."
+  exit 1
 fi
