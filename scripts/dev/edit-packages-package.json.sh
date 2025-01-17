1e623ab051e16b909bbacb7d4e469b87d062a6ee
ACoolmanTelicent <a.coleman@telicent.io>
2025-01-17 10:44:01 +0000

    feat(publishConfig): access: public

--- scripts/dev/edit-packages-package.json.sh
+++ scripts/dev/edit-packages-package.json.sh
@@ -4,13 +4,24 @@ set -e
 for file in packages/*/package.json; do
   [ -f "$file" ] || continue
   echo "Updating $file"
-#   # Use jq to add or overwrite the scripts
-#   jq '.scripts.prepublish = "yarn lint && yarn test" |
-#       .scripts.prepublishOnly = "yarn build"' \
-#     "$file" > "$file.tmp" && mv "$file.tmp" "$file"
-  # Use jq to add or overwrite the scripts
-  jq '.devDependencies.eslint = "^8.56.0"' \
-    "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+  
+  if [ false ]; then
+    # Use jq to add or overwrite the scripts
+    jq '.scripts.prepublish = "yarn lint && yarn test" |
+        .scripts.prepublishOnly = "yarn build"' \
+      "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+  fi
+
+  if [ false ]; then
+    # Use jq to add or overwrite the scripts
+    jq '.devDependencies.eslint = "^8.56.0"' \
+      "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+  fi
+
+  if [ true ]; then
+    jq '.publishConfig.access = "public"' \
+      "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+  fi
 done
 
 echo "Done!"
