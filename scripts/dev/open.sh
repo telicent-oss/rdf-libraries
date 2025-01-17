55ab6ac50d1f108cafe459c6a2860fdbfdb4732f
ACoolmanTelicent <a.coleman@telicent.io>
2025-01-17 14:13:26 +0000

    feat(nx): re-add nx

--- scripts/dev/open.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Example Bash script to open all .js files in the current directory
-for file in ./packages/*/scripts/use-links-to-local-packages.sh; do
-  code "$file" -r  # -r flag to reuse the window
-done
