bac84806f594b0ea79bfa33b221e8791953c2be9
davec504 <88431810+davec504@users.noreply.github.com>
2025-07-18 15:21:13 +0100

    fix:Telfe 1037/alter deps (#179)
    * fix: change ds dep to dev dep
    
    * fix: change ds dep to dev dep

--- packages/ontology-icon-react-lib/vite.config.ts
+++ packages/ontology-icon-react-lib/vite.config.ts
@@ -1,20 +1,29 @@
 import { resolve } from "path";
 import { defineConfig, PluginOption } from "vite";
 import dts from "vite-plugin-dts";
-import packageJSON from './package.json';
+import path from "path";
+import fs from "fs";
 
-const dtsPlugin = dts({ insertTypesEntry: true }) as unknown as PluginOption
+const packageJson = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "package.json"), "utf-8")
+);
+
+const peerDeps = packageJson.peerDependencies
+  ? Object.keys(packageJson.peerDependencies)
+  : [];
+
+const dtsPlugin = dts({ insertTypesEntry: true }) as unknown as PluginOption;
 export default defineConfig({
   build: {
     minify: false,
     sourcemap: true,
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: '@telicent-oss/ontology-icon-react-lib',
-      fileName: 'index'
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "@telicent-oss/ontology-icon-react-lib",
+      fileName: "index",
     },
     rollupOptions: {
-      external: Object.keys(packageJSON.dependencies || {}),
+      external: peerDeps,
     },
   },
   plugins: [dtsPlugin],
