22f5386aca65845275f1b0deadd0445c26dfc2e4
ACoolmanTelicent <a.coleman@telicent.io>
2025-09-05 16:49:01 +0100

    fix(publish:ci): explain diff

--- scripts/lerna/publish:ci.sh
+++ scripts/lerna/publish:ci.sh
@@ -15,8 +15,8 @@ yarn lerna run build --stream --no-prefix --concurrency 1  --include-dependencie
 
 # 4. Check for uncommitted changes
 if ! git diff-index --quiet HEAD --; then
-  git status --short
-  echo "Error: Uncommitted changes detected. Commit or stash them before publishing."
+  git_status=$(git status --short)
+  echo "Error: Uncommitted changes detected. Commit or stash them before publishing: $git_status"
   exit 1
 fi
 
